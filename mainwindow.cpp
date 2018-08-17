fdb68e887ff2f35f0b0a3ca1a969cd9ff64a7f6c
Rakib-hosen <38634021+Rakib-hosen@users.noreply.github.com>
2018-08-17 21:46:58 +0600

    minor upgrade

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,14 +11,6 @@ MainWindow::MainWindow(QWidget *parent) :
     QTimer *timer=new QTimer(this);
     connect(timer,SIGNAL(timeout()),this,SLOT(showTime()));
     timer->start(1000);
-    /*showTime(){
-
-        QTime time = QTime::currentTime();
-        QString time_text= time.toString("hh : mm : ss");
-        ui->Digital_clock->setText(time_text);
-        }*/
-
-
 }
 void MainWindow::showTime(){
 
